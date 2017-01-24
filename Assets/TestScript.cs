8cb9528574ed5117cd6f4dab75ef4fcce4cf8eae
Nickolay Shmyrev <nshmyrev@gmail.com>
2017-01-24 04:46:13 +0300

    First import

---
+++ Assets/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class TestScript : MonoBehaviour {
+
+	[DllImport ("libc.dylib")]
+	private static extern int getpid ();
+
+	void Start () {
+		Debug.Log ("Started");
+	}
+
+	public void OnMyClick() {
+		Debug.Log ("Onclick " + getpid() + " here");
+	}
+}
