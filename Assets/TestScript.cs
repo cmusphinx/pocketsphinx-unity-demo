729f9672ca97fe8c9e84652784ce9df62df7d097
Nickolay Shmyrev <nshmyrev@gmail.com>
2017-01-26 16:56:16 +0300

    Pocketsphinx works

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Runtime.InteropServices;
+using Pocketsphinx;
+using System.IO;
 
+[RequireComponent(typeof(AudioSource))]
 public class TestScript : MonoBehaviour {
 
-	[DllImport ("libc.dylib")]
-	private static extern int getpid ();
+	AudioSource source;
+	Decoder d;
 
 	void Start () {
 		Debug.Log ("Started");
+
+		source = GetComponent<AudioSource>();
+		source.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+		source.mute = true;
+		source.Play ();
+		SetupDecoder ();
 	}
 
-	public void OnMyClick() {
-		Debug.Log ("Onclick " + getpid() + " here");
+	void SetupDecoder() {
+		Config c = Decoder.DefaultConfig();
+		c.SetString("-hmm", "/usr/local/share/pocketsphinx/model/en-us/en-us");
+		c.SetString("-lm", "/usr/local/share/pocketsphinx/model/en-us/en-us.lm.bin");
+		c.SetString("-dict", "/usr/local/share/pocketsphinx/model/en-us/cmudict-en-us.dict");
+		Decoder d = new Decoder(c);
+
+		byte[] data = File.ReadAllBytes("goforward.raw");
+		d.StartUtt();
+		d.ProcessRaw(data, data.Length, false, false);
+		d.EndUtt();
+
+		Debug.Log(string.Format("Result is '{0}'", d.Hyp().Hypstr));
+
+		foreach (Segment s in d.Seg()) {
+			Debug.Log(s);
+		}
+
+	}
+
+	void Update() {
+		float[] data = new float[2048];
+		source.GetOutputData (data, 0);
+	//	Debug.Log ("Captured data");
 	}
 }
