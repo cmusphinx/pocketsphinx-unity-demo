a23ac889a9008153d3c1e9b3977cd97c96cd8bcc
Nickolay Shmyrev <nshmyrev@gmail.com>
2017-01-26 19:37:26 +0300

    Actually react on keypharse

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -3,46 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 using Pocketsphinx;
 using System.IO;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(AudioSource))]
 public class TestScript : MonoBehaviour {
 
 	AudioSource source;
+	Text text;
 	Decoder d;
+
+	int countdown;
+	int MAX_COUNTDOWN = 50;
 
 	void Start () {
 		Debug.Log ("Started");
 
-		source = GetComponent<AudioSource>();
-		source.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
-		source.mute = true;
-		source.Play ();
 		SetupDecoder ();
+
+		source = GetComponent<AudioSource>();
+		source.volume = 0.05f; // Important because we can not mute
+		source.clip = Microphone.Start(null, true, 10, 44100);
+		while (Microphone.GetPosition (null) <= 0) {	
+			// Waiting
+		}
+		source.Play ();
+
+		text = GetComponentInChildren<Text> ();
+
 	}
 
 	void SetupDecoder() {
 		Config c = Decoder.DefaultConfig();
-		c.SetString("-hmm", "/usr/local/share/pocketsphinx/model/en-us/en-us");
-		c.SetString("-lm", "/usr/local/share/pocketsphinx/model/en-us/en-us.lm.bin");
-		c.SetString("-dict", "/usr/local/share/pocketsphinx/model/en-us/cmudict-en-us.dict");
-		Decoder d = new Decoder(c);
+		c.SetString ("-hmm", "/usr/local/share/pocketsphinx/model/en-us/en-us");
+		c.SetString ("-dict", "/usr/local/share/pocketsphinx/model/en-us/cmudict-en-us.dict");
 
-		byte[] data = File.ReadAllBytes("goforward.raw");
-		d.StartUtt();
-		d.ProcessRaw(data, data.Length, false, false);
-		d.EndUtt();
+		c.SetString ("-keyphrase", "oh unity");
+		c.SetFloat ("-kws_threshold", 1e-30);
 
-		Debug.Log(string.Format("Result is '{0}'", d.Hyp().Hypstr));
+		c.SetFloat ("-samprate", 44100);
+		c.SetInt ("-nfft", 2048);
 
-		foreach (Segment s in d.Seg()) {
-			Debug.Log(s);
-		}
-
+		d = new Decoder(c);
+		d.StartUtt ();
 	}
 
 	void Update() {
-		float[] data = new float[2048];
-		source.GetOutputData (data, 0);
-	//	Debug.Log ("Captured data");
+		if (countdown > 0) {
+			Debug.Log ("Countdown " + countdown);
+
+			if (countdown == MAX_COUNTDOWN) {
+				text.text = "Yes master";
+			}
+			countdown--;
+			if (countdown == 0) {
+				text.text = "Say \"oh unity\"";
+			}
+
+		}
 	}
+		
+	static byte[] convertToBytes (float[] data, int channels)
+	{
+		byte[] byteData = new byte[data.Length / channels * 2];
+		for (int i = 0; i < data.Length / channels; i++) {
+			float sum = 0;
+			for (int j = 0; j < channels; j++) {
+				sum += data [i * channels + j];
+			}
+			short val = (short)(sum / channels * 20000);
+			byteData [2 * i] = (byte) (val & 0xff);
+			byteData [2 * i + 1] = (byte) (val >> 8);
+		}
+		return byteData;
+	}
+
+	void OnAudioFilterRead(float[] data, int channels) {
+
+		byte[] byteData = convertToBytes (data, channels);
+
+		d.ProcessRaw(byteData, byteData.Length, false, false);
+		// Start countdown if hypothesis detected
+		if (d.Hyp() != null) {
+			Debug.Log ("Detected keyword");
+			d.EndUtt ();
+			d.StartUtt ();
+
+			countdown = MAX_COUNTDOWN;
+		}
+	}
+
 }
