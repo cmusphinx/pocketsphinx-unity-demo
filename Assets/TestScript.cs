92d61d5b2628f9daeca005f256b6c8832c805120
Nickolay Shmyrev <nshmyrev@gmail.com>
2017-02-14 19:07:03 +0300

    Proper mute of input for Unity 5

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using Pocketsphinx;
 using System.IO;
 using UnityEngine.UI;
+using System;
 
 [RequireComponent(typeof(AudioSource))]
 public class TestScript : MonoBehaviour {
 
-	AudioSource source;
+	AudioSource source_mic;
+	AudioSource source_reply;
+
 	Text text;
 	Decoder d;
 
@@ -20,22 +24,27 @@ public class TestScript : MonoBehaviour {
 
 		SetupDecoder ();
 
-		source = GetComponent<AudioSource>();
-		source.volume = 0.05f; // Important because we can not mute
-		source.clip = Microphone.Start(null, true, 1, 44100);
-		while (Microphone.GetPosition (null) <= 0) {	
-			// Waiting
-		}
-		source.Play ();
+		AudioSource [] sources = GetComponents<AudioSource>();
+		source_mic = sources [0];
+		source_reply = sources [1];
+
+		source_mic.clip = Microphone.Start(null, true, 1, 44100);
+		source_mic.Play ();
 
 		text = GetComponentInChildren<Text> ();
 
 	}
 
 	void SetupDecoder() {
+		Debug.Log("!!!!!! Initializing decoder");
 		Config c = Decoder.DefaultConfig();
-		c.SetString ("-hmm", "/usr/local/share/pocketsphinx/model/en-us/en-us");
-		c.SetString ("-dict", "/usr/local/share/pocketsphinx/model/en-us/cmudict-en-us.dict");
+		if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer) {
+			c.SetString ("-hmm", "/usr/local/share/pocketsphinx/model/en-us/en-us");
+			c.SetString ("-dict", "/usr/local/share/pocketsphinx/model/en-us/cmudict-en-us.dict");
+		} else if (Application.platform == RuntimePlatform.Android) {
+			c.SetString ("-hmm", "/sdcard/Android/data/edu.cmu.sphinx.pocketsphinx/files/sync/en-us-ptm");
+			c.SetString ("-dict", "/sdcard/Android/data/edu.cmu.sphinx.pocketsphinx/files/sync/cmudict-en-us.dict");
+		}
 		//c.SetString ("-rawlogdir", "/tmp");
 
 		c.SetString ("-keyphrase", "oh unity");
@@ -46,6 +55,7 @@ public class TestScript : MonoBehaviour {
 
 		d = new Decoder(c);
 		d.StartUtt ();
+		Debug.Log ("!!!!! Done initializing decoder");
 	}
 
 	void Update() {
@@ -54,10 +64,12 @@ public class TestScript : MonoBehaviour {
 
 			if (countdown == MAX_COUNTDOWN) {
 				text.text = "Yes master";
+				source_reply.Play ();
 			}
 			countdown--;
 			if (countdown == 0) {
 				text.text = "Say \"oh unity\"";
+				source_mic.Play ();
 			}
 
 		}
@@ -65,16 +77,19 @@ public class TestScript : MonoBehaviour {
 		
 	static byte[] convertToBytes (float[] data, int channels)
 	{
+		float tot = 0;
 		byte[] byteData = new byte[data.Length / channels * 2];
 		for (int i = 0; i < data.Length / channels; i++) {
 			float sum = 0;
 			for (int j = 0; j < channels; j++) {
 				sum += data [i * channels + j];
 			}
-			short val = (short)(sum / channels * 20000 / 0.05); // volume
+			tot += sum * sum;
+			short val = (short)(sum / channels * 20000); // volume
 			byteData [2 * i] = (byte) (val & 0xff);
 			byteData [2 * i + 1] = (byte) (val >> 8);
 		}
+		//Debug.Log (Math.Sqrt(tot / data.Length / channels));
 		return byteData;
 	}
 
